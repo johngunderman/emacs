f64462a3a02ba931e72d851e931678850b74664f
John Gunderman <johngunderman@gmail.com>
2009-09-02 12:07:26 -0400

    Added some nice settings.

--- emacs-john.el
+++ emacs-john.el
@@ -3,8 +3,13 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-'(inhibit-startup-screen t)
-'(haskell-program-name "/usr/local/bin/ghci")
+ '(column-number-mode t)
+ '(display-battery-mode t)
+ '(display-time-mode t)
+ '(haskell-program-name "/usr/local/bin/ghci")
+ '(indicate-empty-lines t)
+ '(inhibit-startup-screen t)
+ '(transient-mark-mode t)
 )
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
@@ -39,6 +44,8 @@
 (setq x-select-enable-clipboard t)
 
 (tool-bar-mode -1) ;turn off the toolbar.
+
+(toggle-scroll-bar -1) ;turn off the scroll bar
 
 (delete-selection-mode t)  ;delete a selection with a keypress
 
